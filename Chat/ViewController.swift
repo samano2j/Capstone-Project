838778cc82f6bc6f584c653979ef1136c74005c6
user149302 <user149302@la496.local>
2019-02-28 18:04:43 -0800

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by user149302 on 2/28/19.
+//  Copyright © 2019 user149302. All rights reserved.
+//
+
+import UIKit
+import PusherChatkit
+
+class ViewController: UIViewController {
+    var chatManager: ChatManager!
+    var currentUser: PCCurrentUser?
+    var chatManagerDelegate: PCChatManagerDelegate?
+    var messages: [PCMessage] = []
+    var defaultFrame: CGRect!
+    
+    @IBOutlet weak var messageInput: UITextField!
+    @IBOutlet weak var messagesTable: UITableView!
+    
+    @IBAction func SendMessage(_ sender: UIButton) {
+      
+        self.currentUser!.sendMessage(
+            roomID: self.currentUser!.rooms.first!.id,
+            text: self.messageInput.text!
+        ) { messageId, error in
+            guard error == nil else {
+                print("Error sending message: \(error!.localizedDescription)")
+                return
+            }
+            print("Message sent!")
+        }
+        
+        self.messageInput.text!.removeAll()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messagesTable.transform = CGAffineTransform(scaleX: 1, y: -1)
+        defaultFrame = self.view.frame
+        messagesTable.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        messagesTable.dataSource = self
+
+        chatManager = ChatManager(
+            instanceLocator: "v1:us1:c8b9b762-7de0-4e17-b597-b452e7659fa4",
+            tokenProvider: PCTokenProvider(url: "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c8b9b762-7de0-4e17-b597-b452e7659fa4/token"),
+            userID: "iden"
+        )
+        
+        self.chatManagerDelegate = MyChatManagerDelegate()
+        
+        chatManager.connect(
+            delegate: self.chatManagerDelegate!
+        ) { [unowned self] currentUser, error in
+            guard error == nil else {
+                print("Error connecting: \(error!.localizedDescription)")
+                return
+            }
+            print("Connected!")
+            
+            guard let currentUser = currentUser else { return }
+            self.currentUser = currentUser
+            
+            currentUser.subscribeToRoom(
+                room: currentUser.rooms[0],
+                roomDelegate: self
+            ) { err in
+                guard error == nil else {
+                    print("Error subscribing to room: \(error!.localizedDescription)")
+                    return
+                }
+                print("Subscribed to room!")
+            }
+            
+        }
+        
+        
+        
+    }
+    
+   
+}
+
+class MyChatManagerDelegate: PCChatManagerDelegate {}
+
+extension ViewController: PCRoomDelegate {
+    func onMessage(_ message: PCMessage) {
+        print("\(message.sender.id) sent \(message.text)")
+        messages.insert(message, at: 0)
+        DispatchQueue.main.async {
+            self.messagesTable.reloadData()
+        }
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.contentView.transform = CGAffineTransform(scaleX: 1, y: -1)
+        let message = self.messages[indexPath.row]
+        cell.textLabel?.text = "\(message.sender.displayName): \(message.text)"
+        return cell
+    }
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Chat' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Chat
+
+  target 'ChatTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ChatUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  pod 'PusherChatkit'
+
+end
